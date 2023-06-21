a6529de5b8a0d902f79cf7c01da50d2e92493f97
Abhishek Shewale <abhishewale100@gmail.com>
2023-06-21 20:49:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./Components/navbar.css";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
-function App() {
+const Home = () => <h1>Home Page</h1>;
+const About = () => <h1>About Page</h1>;
+const Services = () => <h1>Services Page</h1>; // Changed from Service to Services
+const Jobs = () => <h1>Jobs Page</h1>;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} /> // Changed from
+        /service to /services
+        <Route path="/jobs" element={<Jobs />} />
+      </Routes>
+    </Router>
+  );
+};
+
+const Navbar = () => {
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
+  return (
+    <div>
+      <nav className="main-nav">
+        {/* 1st logo part */}
+        <div className="logo">
+          <h2>
+            <span>J</span>ob
+            <span>S</span>eeker
+          </h2>
+        </div>
+        {/* 2nd menu part */}
+        <div
+          className={
+            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+          }
         >
-          Learn React
-        </a>
-      </header>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            <li>
+              <Link to="/jobs">Jobs</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
-}
+};
 
 export default App;
